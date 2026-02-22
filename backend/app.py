ffae04ce6d499955ae54c00eb57c94bff9ef0098
ankur <ankurt5202@gmail.com>
2026-02-22 11:00:41 +0530

    commit -05 test

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,18 @@ app = Flask(__name__)
 
 CORS(app, origins=["http://127.0.0.1:5500"])
 
+@app.before_request
+def log_request():
+    print("\n -- Incoming request --")
+    print("Method : " , request.method)
+    print("Path : ", request.path)
+    print("Origin : ", request.headers.get("Origin"))
+    
+@app.after_request
+def log_response(response):
+    print("Status Code : ", response.status_code)
+    return response
+
 @app.route("/test-get", methods=["GET"])
 def test_get():
     return jsonify({"message" : "TESTING GET MESSAGE - SUCCESS"})
